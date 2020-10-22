823a8074a184465eaff7c1da12ec7522ddbc4243
mit-ywtm <62491757+mit-ywtm@users.noreply.github.com>
2020-10-22 23:16:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,3 +7,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+sed -i '$a src-git docker https://github.com/lisaac/luci-lib-docker' feeds.conf.default
+sed -i '$a src-git dockerman  https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
