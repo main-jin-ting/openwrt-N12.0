e9056640a9b925796dc2ba28deb8d27a85eb3425
jin_ting <62491757+main-jin-ting@users.noreply.github.com>
2021-06-07 19:08:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git custom https://github.com/main-jin-ting/openwrt_package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-sed -i '$a src-git docker https://github.com/lisaac/luci-lib-docker' feeds.conf.default
-sed -i '$a src-git dockerman  https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
